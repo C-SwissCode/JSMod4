c7072414a7bc53eae336d2386bac976f327b8d6c
Caleb <cswiss12@protonmail.com>
2020-08-05 17:37:12 -0500

    Completed rolling and holding events

--- app.js
+++ app.js
@@ -9,20 +9,61 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer, diceDOM;
 
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
+diceDOM = document.querySelector('.dice');
 
-dice = Math.floor(Math.random() * 6) + 1;
+/**************************************
+ * Show dice, update current score, erase current score if rolled 1 and switch users, hold button to add current score to total score and swtich user
+ */
 
-console.log(dice);
+//Zero out scores
+var zeroScores = function (classes) {
+  var x = document.querySelectorAll(classes);
+  for (i = 0; i < x.length; i++) {
+    x[i].textContent = 0;
+  }
+}
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
-// document.querySelector('#current-1').innerHTML = '<em>' + dice + '</em>';
+zeroScores('.player-current-score');
+zeroScores('.player-score');
 
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
+//Rolling the dice
+document.querySelector('.btn-roll').addEventListener('click', function () {
 
-document.querySelector('.dice').style.display = 'none';
+  //Update current score if 1 then zero out current and switch player
+  var dice = Math.floor(Math.random() * 6) + 1;
+  if (dice !== 1) {
+    diceDOM.src = 'dice-' + dice + '.png';
+    diceDOM.style.display = 'block';
+    roundScore += dice;
+    document.getElementById('current-' + activePlayer).textContent = roundScore;
+  } else {
+    diceDOM.style.display = 'none';
+    roundScore = 0;
+    document.getElementById('current-' + activePlayer).textContent = roundScore;
+    //Switch active player
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+  }
+})
+
+
+//Holding Score
+document.querySelector('.btn-hold').addEventListener('click', function () {
+  //Update total score and clear current score
+  scores[activePlayer] += roundScore;
+  roundScore = 0;
+  document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+  document.getElementById('current-' + activePlayer).textContent = roundScore;
+
+  //Switch users and hide dice
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+  diceDOM.display = 'none';
+})
